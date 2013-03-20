b2174538ff06e4ab7deb99820ed03e66b279c8a5
Przemek Piotrowski <przemek@przemek-Inspiron.(none)>
2013-03-20 10:20:04 +0100

    Dzialajaca kolejka priorytetowa dla dystansu wierzcholkow

--- Graph.hpp
+++ Graph.hpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 using namespace boost;
-
+using namespace std;
 class Graph
 {
 public:
@@ -28,8 +28,11 @@ public:
 	typedef graph_traits<GraphContainer>::vertex_descriptor Vertex;
 	typedef graph_traits<GraphContainer>::edge_descriptor Edge;
 	
-	typedef typename graph_traits<GraphContainer>::vertex_iterator VertexIter;
-	typedef typename graph_traits<GraphContainer>::edge_iterator EdgeIter;
+	typedef graph_traits<GraphContainer>::vertex_iterator VertexIter;
+	typedef graph_traits<GraphContainer>::edge_iterator EdgeIter;
+	
+	typedef list<Edge> EdgeList;
+	
 	/* constructors etc. */
 	Graph()
 	{}
@@ -52,6 +55,9 @@ public:
 	
 	enum Color{RED, GREEN, BLUE, BLACK};
 	void setEdgeColor(const Edge& e, Color c);
+	
+	EdgeList getShortestPath(const Vertex& v1, const Vertex& v2);
+	bool compVertexDist(const Vertex& v1, const Vertex& v2);
 	
 };
 #endif
